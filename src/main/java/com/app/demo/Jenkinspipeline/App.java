51c691ad65fb37920de242820ff7ca00445cbecf
Hemanth <hemanth22092003@gmail.com>
2023-12-28 15:38:28 +0530

    First commit

---
+++ src/main/java/com/app/demo/Jenkinspipeline/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Jenkinspipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is for Jenkins Pipeline Exerxise 3");
+    }
+}
